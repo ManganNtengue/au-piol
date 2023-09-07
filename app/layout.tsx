3cdfa86c533c452815d9303b1af100c48c7ffc1a
ManganNtengue <mangankevin28@yahoo.com>
2023-09-07 06:41:28 +0100

    F04: Auth Modal UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from "./components/navbar/Navbar"
+import ClientOnly from "./components/ClientOnly"
+import Modal from "./components/modals/Modal"
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -25,7 +27,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar/>
+        <ClientOnly>
+          <Modal actionLabel="Submit" title="Hello World" isOpen/>
+          <Navbar/>
+        </ClientOnly>
+        
         {children}
       </body>
     </html>
